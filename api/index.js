0aa1f24f014bee6eede6163e2cb46b0eee136de8
Shalev <shalevco105@gmail.com>
2024-12-28 13:43:32 +0200

    roll back vercel

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 const cron = require("node-cron");
 const axios = require("axios");
+const express = require("express");
+const app = express();
+const PORT = 3000;
 
 const performAction = () => {
   const url = "https://amiram-server.vercel.app/";
@@ -17,4 +20,15 @@ const performAction = () => {
 cron.schedule("0 22 * * *", performAction);
 cron.schedule("0 16 * * *", performAction);
 cron.schedule("0 20 * * *", performAction);
-setInterval(performAction, 10000); 
+setInterval(performAction, 10000);
+
+app.get("/", (req, res) => {
+  res.send(
+    "Express server is running. Check the logs for the recurring action."
+  );
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  performAction();
+});
