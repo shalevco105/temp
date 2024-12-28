d693b816cfc8590cff3a82913ea0c8f913417c36
Shalev <shalevco105@gmail.com>
2024-12-28 13:13:09 +0200

    request from amiram server

--- api/index.js
+++ api/index.js
@@ -1,20 +1,30 @@
-const express = require('express');
-const cron = require('node-cron');
+const express = require("express");
+const cron = require("node-cron");
 const app = express();
 const PORT = 3000;
 
 const performAction = () => {
-  console.log('Action executed at:', new Date().toISOString());
+  const axios = require("axios");
+  const url = "https://amiram-server.vercel.app/";
+  axios
+    .get(url)
+    .then((response) => {
+      console.log("Response from server:", response.data);
+    })
+    .catch((error) => {
+      console.error("Error occurred while making the request:", error.message);
+    });
+  console.log("Action executed at:", new Date().toISOString());
 };
 
-cron.schedule('0 22 * * *', performAction); 
-cron.schedule('0 16 * * *', performAction); 
-cron.schedule('0 20 * * *', performAction); 
-cron.schedule('9 13 * * *', performAction); 
-cron.schedule('8 13 * * *', performAction); 
+cron.schedule("0 22 * * *", performAction);
+cron.schedule("0 16 * * *", performAction);
+cron.schedule("0 20 * * *", performAction);
+cron.schedule("15 13 * * *", performAction);
+cron.schedule("14 13 * * *", performAction);
 
-app.get('/', (req, res) => {
-  res.send('I am alive');
+app.get("/", (req, res) => {
+  res.send("I am alive");
 });
 
 app.listen(PORT, () => {
